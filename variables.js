fc185838fdb1d3d0ddb7e7ca2666cc219c9b4626
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-09-24 14:56:39 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var movie = "Jaws";
+var isEntertaining = true;
+
+movie = "Star Wars";
+
+var num4 = 4;
+var num6 = 6;
+
+var product = num4 * num6;
+
+favoriteFoods = ["poke", "pad see ew", "fried okra"];
+favoriteFoods.shift;
+
+var numOfLetters = movie.length;
