8000ad2e4462eed4a89aa040d5d8ddcc84bc25b7
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-09-24 15:17:33 -0700

    Create model of developer

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name:"Adam",
+  pronouns: "he/him",
+  yearsWorking: 10,
+  salary$: 200,000,
+  isSkilled: true,
+  usesMac: true,
+  languagesKnow: ["Ruby", "Javascript",],
+  companiesWorkedFor: ["Core-Mark", "GitHub"],
+};
