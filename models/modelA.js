2fdc453bc8096bbeffffc739ab9495560ffba65b
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-09-24 15:10:29 -0700

    Create model of dinosaur

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  color: "green",
+  size: "large",
+  numOfTeeth: 56,
+  heightInFeet: 18,
+  isPredator: true,
+  canSwim: false,
+  regionsItLives: ["China", "Russia", "Canada"],
+  possibleColors: ["black", "green", "brown", "gray"],
+};
